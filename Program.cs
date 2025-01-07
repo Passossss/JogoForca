dd2ad95b676cd302cda63264e2402c447544f0f9
Gustavo Santos dos Passos <guspassos@imap-157501.IMAP_DOMAIN>
2025-01-07 09:57:32 -0300

    firt Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("BEM-VINDO AO JOGO DA FORCA");
+        Console.WriteLine("==========================");
+        LetraSelecionada();
+    }
+    static string palavraSecreta = "Cachorro";
+    static int numTentativas = palavraSecreta.Length;
+    public static void LetraSelecionada()
+    {
+        string? let;
+        Console.WriteLine("Digite uma letra para o jogo da forca: ");
+        let = Console.ReadLine();
+        let = let.ToLower();
+        if (palavraSecreta.ToLower().Contains(let))
+        {
+            Console.WriteLine("A letra {0} existe na palavra secreta!", let);
+        }
+        else
+        {
+            Console.WriteLine("A letra {0} NÃO existe na palavra!", let);
+        }
+    }
+
+
+
+}
