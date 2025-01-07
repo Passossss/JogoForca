3d8b32d75a23ba495642c41c391bfc2a35e962a3
Gustavo Santos dos Passos <guspassos@imap-157501.IMAP_DOMAIN>
2025-01-07 10:22:55 -0300

    all code finaly

--- Program.cs
+++ Program.cs
@@ -1,30 +1,120 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
+    static string palavraSecreta = "Cachorro";
+    static string? palavraDescoberta;
+    static int numTentativas;
+    static string letrasErradas = "";
 
     static void Main(string[] args)
     {
+        palavraDescoberta = new string('_', palavraSecreta.Length);
+        numTentativas = 8;
         Console.WriteLine("BEM-VINDO AO JOGO DA FORCA");
         Console.WriteLine("==========================");
-        LetraSelecionada();
-    }
-    static string palavraSecreta = "Cachorro";
-    static int numTentativas = palavraSecreta.Length;
-    public static void LetraSelecionada()
-    {
-        string? let;
-        Console.WriteLine("Digite uma letra para o jogo da forca: ");
-        let = Console.ReadLine();
-        let = let.ToLower();
-        if (palavraSecreta.ToLower().Contains(let))
+        ExibirPalavraDescoberta();
+
+        while (numTentativas > 0 && palavraDescoberta.Contains('_'))
         {
-            Console.WriteLine("A letra {0} existe na palavra secreta!", let);
+            LetraSelecionada();
+            ExibirPalavraDescoberta();
+            ExibirBonecoForca();
+        }
+
+        if (palavraDescoberta == palavraSecreta)
+        {
+            Console.WriteLine("Parabéns, você acertou a palavra secreta!");
         }
         else
         {
-            Console.WriteLine("A letra {0} NÃO existe na palavra!", let);
+            Console.WriteLine($"Você perdeu! A palavra secreta era: {palavraSecreta}");
         }
     }
 
+    static void ExibirPalavraDescoberta()
+    {
+        Console.WriteLine("\nPalavra: " + palavraDescoberta);
+        Console.WriteLine("Letras erradas: " + letrasErradas);
+        Console.WriteLine($"Tentativas restantes: {numTentativas}");
+    }
 
+    static void ExibirBonecoForca()
+    {
+        switch (numTentativas)
+        {
+            case 6:
+                break;
+            case 5:
+                Console.WriteLine(" |   ");
+                break; 
+            case 4:
+                Console.WriteLine(" O   ");
+                break; 
+            case 3:
+                Console.WriteLine(" O   ");
+                Console.WriteLine(" |   ");
+                break;
+            case 2:
+                Console.WriteLine(" O   ");
+                Console.WriteLine(" |   ");
+                Console.WriteLine("/    ");
+                break;
+            case 1:
+                Console.WriteLine(" O   ");
+                Console.WriteLine(" |   ");
+                Console.WriteLine("/ \\  ");
+                break;
+            case 0:
+                Console.WriteLine(" O   ");
+                Console.WriteLine("/|\\ ");
+                Console.WriteLine(" |   ");
+                Console.WriteLine("/ \\  ");
+                break;
+        }
+    }
 
+    static void LetraSelecionada()
+    {
+        string? let;
+        Console.Write("Digite uma letra: ");
+        let = Console.ReadLine();
+        let = let.ToLower();
+
+        if (let.Length != 1 || !Char.IsLetter(let[0]))
+        {
+            Console.WriteLine("Por favor, digite apenas uma letra.");
+            return;
+        }
+
+        if (letrasErradas.Contains(let) || palavraDescoberta.Contains(let))
+        {
+            Console.WriteLine($"Você já tentou a letra '{let}'.");
+            return;
+        }
+
+        if (palavraSecreta.ToLower().Contains(let))
+        {
+            Console.WriteLine($"A letra '{let}' existe na palavra!");
+            AtualizarPalavraDescoberta(let);
+        }
+        else
+        {
+            Console.WriteLine($"A letra '{let}' NÃO existe na palavra!");
+            letrasErradas += let + " ";
+            numTentativas--;
+        }
+    }
+    static void AtualizarPalavraDescoberta(string letra)
+    {
+        char[] palavraArray = palavraDescoberta.ToCharArray();
+        for (int i = 0; i < palavraSecreta.Length; i++)
+        {
+            if (palavraSecreta[i].ToString().ToLower() == letra)
+            {
+                palavraArray[i] = palavraSecreta[i];
+            }
+        }
+        palavraDescoberta = new string(palavraArray);
+    }
 }
